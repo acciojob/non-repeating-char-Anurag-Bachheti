6789880e84c0abd64e763e12dd6949f5ea8a174a
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-24 00:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i - 1] && str[i] !== str[i + 1]) {
+            return str[i]; // Return the first character that breaks the repetition chain
+        }
+    }
+    return null; // If no such character is found
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
